8e1608c3b4f581d8304912fb9f9a00a230138efb
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-03-04 14:00:25 +0100

    generator finished, max hardcoded

--- Generator.py
+++ Generator.py
@@ -1,12 +1,33 @@
 __author__ = 'spijs'
 
+from random import randint
+
 class Generator:
+    max_t = 50
+    max_demand = 5
+
     def __init__(self):
-        pass
+        self.t = randint(0,self.max_t)
 
     def generate_general(self):
-        pass
 
+        d_min = randint(0,self.t)
+        d_max = randint(d_min,self.t)
+        o_min = randint(0,self.t)
+        o_max = randint(o_min,self.t)
+        b = self.generate_array(self.t,self.max_demand)
+
+        print ('t = %s, d^- = %s, d^+ = %s, o^- = %s, o^+ = %s' % (str(self.t), str(d_min), str(d_max), str(o_min),str(o_max)))
+        print ('b = %s' % b)
 
     def generate_cyclic(self):
-        pass
+        ass = self.generate_array(self.t,1)
+        b = self.generate_array(self.t,self.max_demand)
+        print('assignment = %s' % ass)
+        print('b = %s' % b)
+
+    def generate_array(self,t,max):
+        b = []
+        for i in range(t):
+            b.append(randint(0,max))
+        return b

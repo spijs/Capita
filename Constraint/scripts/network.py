baeb887f34e1fe2d188f2026fcc5027c91406e43
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-05-03 14:25:18 +0200

    drie types regressie?

--- Constraint/scripts/network.py
+++ Constraint/scripts/network.py
@@ -5,6 +5,7 @@ from sknn.mlp import *
 import argparse
 import pickle
 from Regressor import Regressor as Reg
+from SVMRegressor import SVMRegressor, LinearRegressor
 class Network(Reg):
 
     def __init__(self, nbOfLayers, learning_rate,nb_iter,valid_input,valid_output,hidden):
@@ -27,7 +28,7 @@ class Network(Reg):
         return self.nn.predict(train)
 
 
-def run_network(params):
+def run_regression(params):
     train_x,train_y = getData('train')
     print train_x.shape
     print train_y.shape
@@ -35,16 +36,22 @@ def run_network(params):
     print val_x.shape
     print val_y.shape
     test_x,test_y = getData('test')
-    nn = Network(params['layers'],params['learning_rate'],params['iterations'],val_x,val_y,params['hidden'])
-    nn.train(train_x,train_y)
-    pickle.dump(nn,open('learned_network.p','wb'))
+    if params['type']=='network':
+        reg = Network(params['layers'],params['learning_rate'],params['iterations'],val_x,val_y,params['hidden'])
+    elif params['type']=='svm':
+        reg = SVMRegressor()
+    else:
+        reg = LinearRegressor()
+    reg.train(train_x,train_y)
+    pickle.dump(reg,open('learned_network.p','wb'))
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--learning_rate', dest='learning_rate',type=float, default=.01, help='learning rate to be used')
     parser.add_argument('-hi', '--hidden', dest='hidden', type=int, default=256, help='Number of nodes in hidden layer')
-    parser.add_argument('-i', '--iterations', dest='iterations', type=int, default= 10000, help='Number of iterations for training the network')
+    parser.add_argument('-i', '--iterations', dest='iterations', type=int, default= 2000, help='Number of iterations for training the network')
     parser.add_argument('-l', '--layers',dest='layers',type=int, default=5, help='number of hidden layers used')
+    parser.add_argument('-t', '--type', dest='type', type=string, default='network',help = 'type of regression used')
     args = parser.parse_args()
     params = vars(args) # convert to ordinary dict
-    run_network(params)
+    run_regression(params)

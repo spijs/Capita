ab40c7fe96afd0c0812af99f96b6e92f68cd4b40
Wout <w.vekemans@gmail.com>
2016-05-04 16:05:19 +0200

    moar prints

--- Constraint/runitall.py
+++ Constraint/runitall.py
@@ -96,6 +96,7 @@ if __name__ == '__main__':
     for (i,f) in enumerate(f_instances):
         data_forecasts = preds[i]
         data_actual = actuals[i]
+        print "data actual ", data_actual
         (timing, out) = runcheck.mzn_run(args.file_mzn, f, data_forecasts,
                                 tmpdir, mzn_dir=args.mzn_dir,
                                 print_output=args.print_output,

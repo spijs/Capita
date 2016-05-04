8cdecf3bc68456055ebec7d2eed222a1f67620d0
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-05-04 17:33:55 +0200

    hoe zlefs

--- Constraint/scripts/SVMRegressor.py
+++ Constraint/scripts/SVMRegressor.py
@@ -115,6 +115,6 @@ def plot_preds(modelpreds, y_test):
 def get_test_days():
     result = []
     for i in range(7,792): # loop over all days in dataset
-        if i % 84 ==0:
+        if (i-7) % 84 ==24:
             result.append(i)
     return result

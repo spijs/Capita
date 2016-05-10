c04b7bf71a82c42d892ab78a4ede4f4e60cad65e
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-05-10 15:29:52 +0200

    prinsjes

--- Constraint/scripts/Regressor.py
+++ Constraint/scripts/Regressor.py
@@ -165,7 +165,8 @@ def get_data_for_day(prev,column_features,column_prev_features,column_predict,da
             else:
                 row = additional_info_test[i-j]
             extra = extra + row
-        X_TEST.append(X_test[i]+extra)
+        print extra
+        X_TEST[i]=X_test[i]+extra
     print 'X test size:', np.array(X_TEST[0]).shape
     return X_TEST,X,Y_test,y_train
 

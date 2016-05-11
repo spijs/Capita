64b69325a7df8ac42031c6d00c57d9fed3c5118f
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-05-11 15:59:24 +0200

    prinsjes

--- Constraint/scripts/Regressor.py
+++ Constraint/scripts/Regressor.py
@@ -166,6 +166,7 @@ def get_data_for_day(classifier,prev,column_features,column_prev_features,column
     y_train = [eval(row[column_predict]) for row in rows_before_test]
     additional_info = [[eval(v) for (k, v) in row.iteritems() if k in column_prev_features] for row in rows_before_test]
     X = []
+    print classifier
     classifications = classifier.predict(X_train)
     print np.array(X_train).shape
     for i in range(prev*48,len(X_train)):

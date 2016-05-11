8505f47fca4d4ec573e199a608f5747a79306b2f
Thijs Dieltjens <thijsdieltjens@gmail.com>
2016-05-11 16:13:02 +0200

    prints

--- Constraint/scripts/network.py
+++ Constraint/scripts/network.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     parser.add_argument('--use_classifier',dest='classifier',type=bool,default=False,help='use classifier True/False')
     args = parser.parse_args()
     params = vars(args) # convert to ordinary dict
-    print params['use_classifier']
+    print params['classifier']
     if params['comp']:
         compare(params)
     else:
